44e93ddd346553ddcc4c6925612b39fcde3cf2ed
SehyeonOh <sehyeon@dnotitia.com>
2024-12-31 05:00:44 +0000

    add constructor

--- data2cpp/common/data2cpp.hh
+++ data2cpp/common/data2cpp.hh
@@ -4,7 +4,8 @@
 namespace data2cpp {
     class Data2Cpp {
     public:
-        Data2Cpp(std::string source_file, std::string column_name = "") : source_file_(source_file), column_name_(column_name) {}
+        Data2Cpp(std::string source_file) : source_file_(source_file) {}
+        Data2Cpp(std::string source_file, std::string column_name) : source_file_(source_file), column_name_(column_name) {}
         ~Data2Cpp();
         // only support fixed sized list array for now
         virtual const uint8_t *GetRawData(int64_t row_index = 0) = 0;
@@ -17,8 +18,8 @@ namespace data2cpp {
         const float *GetFloatData(int64_t row_index = 0);
         void SetFloatData(const uint8_t *data);
     protected:
-        std::string source_file_;
-        std::string column_name_;
+        std::string source_file_ = "";
+        std::string column_name_ = "";
 
         const float* float_data_;
         bool owns_data_ = false;
